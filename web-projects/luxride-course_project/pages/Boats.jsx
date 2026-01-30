13230ba0ebeefd2de5d5a2c97f586db8c3999bc4
Sohaib_ali <165940210+sohaib-ali47@users.noreply.github.com>
2026-01-30 18:09:18 +0000

    Add files via upload

---
+++ web-projects/luxride-course_project/pages/Boats.jsx
@@ -0,0 +1,75 @@
+function Boats() {
+  return (
+    <center>
+      <br /><br />
+      <h1>Luxury Boats Collection</h1>
+      <hr />
+      <h4>Sail the world's most beautiful waters in vessels that redefine maritime luxury.</h4>
+      <br />
+
+      {/* 1. Assesta III */}
+      <h2>Motor Yacht Assesta III</h2>
+      <img src="images/boat/new (10).jpg" width="1000" alt="Feadship" className="hover-img" />
+      <p>
+        <b>Length:</b> 140 ft<br />
+        <b>Max Speed:</b> 22 knots<br />
+        <b>Features:</b> Jacuzzi, Luxury suites, Entertainment lounge<br />
+        <b>Description:</b> The Assesta III by Feadship combines sleek design with top-tier performance. 
+        Perfect for intimate luxury cruises, it offers elegance, comfort, and advanced maritime technology.
+      </p>
+      <hr />
+
+      {/* 2. Alpha */}
+      <h2>Lürssen Superyacht Alpha</h2>
+      <img src="images/boat/new (8).jpg" width="1000" alt="Lurssen" className="hover-img" />
+      <p>
+        <b>Length:</b> 350 ft<br />
+        <b>Max Speed:</b> 89 knots<br />
+        <b>Capacity:</b> 65 passengers<br />
+        <b>Description:</b> The Lürssen Superyacht Alpha offers unparalleled luxury on the seas. 
+        Its elegant design and state-of-the-art amenities make it perfect for elite voyages.
+      </p>
+      <hr />
+
+      {/* 3. Azure Whisper */}
+      <h3>Azure Whisper</h3>
+      <img src="images/boat/new (11).jpg" width="1000" alt="Azure Whisper Yacht" className="hover-img" />
+      <p>
+        <b>Color:</b> Blue & Turquoise<br />
+        <b>Length:</b> ~90 ft<br />
+        <b>Max Speed:</b> 20 knots<br />
+        <b>Description:</b> The Azure Whisper is a stylish mid-sized yacht in blue and turquoise tones. 
+        Designed for intimate luxury trips, it features advanced communication antennas and a private deck.
+      </p>
+      <hr />
+
+      {/* 4. Obsidian Horizon */}
+      <h3>Obsidian Horizon</h3>
+      <img src="images/boat/new (12).jpg" width="1000" alt="Obsidian Horizon Superyacht" className="hover-img" />
+      <p>
+        <b>Color:</b> Sleek Black<br />
+        <b>Length:</b> ~180 ft<br />
+        <b>Max Speed:</b> 28 knots<br />
+        <b>Features:</b> Helipad on top deck, 4 floors, swimming pool<br />
+        <b>Description:</b> The Obsidian Horizon is a massive 4-floor superyacht with a black exterior. 
+        It combines luxury and performance with a rooftop helipad, pool, and multiple sleeping quarters.
+      </p>
+      <hr />
+
+      {/* 5. White Sentinel */}
+      <h3>White Sentinel</h3>
+      <img src="images/boat/new (13).jpg" width="1000" alt="White Sentinel Yacht" className="hover-img" />
+      <p>
+        <b>Color:</b> White (navy-inspired)<br />
+        <b>Length:</b> ~100 ft<br />
+        <b>Max Speed:</b> 22 knots<br />
+        <b>Description:</b> The White Sentinel draws inspiration from naval vessels, offering a secure and functional private yacht. 
+        Its white hull and multiple antennas ensure excellent communication capabilities.
+      </p>
+      <hr />
+
+    </center>
+  );
+}
+
+export default Boats;
